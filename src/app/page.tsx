e1f4dfbf7d49d0783c64aff189e8fc9f008b54ff
savashn <savashn@proton.me>
2024-12-19 10:11:44 +0300

    fix error at homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Header from "@/components/partials/Header";
 import styles from "./page.module.css";
-import postStyles from '@/styles/post.module.css';
 import { Post } from "@/types/interfaces";
 import Link from "next/link";
 import Image from "next/image";
 import ContentProcessor from "@/utils/ContentProcessor";
+import { notFound } from "next/navigation";
 
 export default async function Home() {
   const api = process.env.API_URI;
@@ -13,76 +13,83 @@ export default async function Home() {
 
   const data = await res.json();
 
+  if (!res.ok) {
+    console.error('API response:', res.status, await res.text());
+    notFound();
+  }
+
   const posts: Post[] = ContentProcessor.processPosts(data);
 
   return (
     <>
       <Header />
 
-      <div className={styles.home}>
+      {data && (
+        <div className={styles.home}>
 
-        <div className={styles.h1}>
-          JOIN TO TURN YOUR THOUGHTS INTO WORDS
-        </div>
+          <div className={styles.h1}>
+            JOIN TO TURN YOUR THOUGHTS INTO WORDS
+          </div>
 
-        <hr />
+          <hr />
 
-        <div className={styles.posts}>
+          <div className={styles.posts}>
 
-          {posts.map((post, idx) => (
-            <div key={idx} className={styles.card}>
+            {posts.map((post, idx) => (
+              <div key={idx} className={styles.card}>
 
-              <div className={styles.cardHeader}>
+                <div className={styles.cardHeader}>
 
-                <Link href={`/user/${post.username}`} className={styles.link}>
+                  <Link href={`/user/${post.username}`} className={styles.link}>
 
-                  <div className={styles.pictureWrapper}>
-                    <Image
-                      src="/profile.jpg"
-                      width={500}
-                      height={500}
-                      alt="Picture of the author"
-                      className={styles.picture}
-                    />
-                  </div>
+                    <div className={styles.pictureWrapper}>
+                      <Image
+                        src="/profile.jpg"
+                        width={500}
+                        height={500}
+                        alt="Picture of the author"
+                        className={styles.picture}
+                      />
+                    </div>
 
-                </Link>
+                  </Link>
 
-                <Link href={`/user/${post.username}`} className={styles.author}>
+                  <Link href={`/user/${post.username}`} className={styles.author}>
 
-                  {post.author}
+                    {post.author}
 
-                </Link>
+                  </Link>
 
-                <p className={styles.date}>
-                  {new Date(post.createdAt).toLocaleDateString('en-GB')}
-                </p>
+                  <p className={styles.date}>
+                    {new Date(post.createdAt).toLocaleDateString('en-GB')}
+                  </p>
+
+                </div>
+
+                <hr />
+
+                <div className={styles.cardBody}>
+
+                  <Link href={`/user/${post.username}/posts/${post.slug}`} className={styles.link}>
+
+                    <h2 className={styles.postHeader}>{post.header}</h2>
+                    <h3 style={{ textDecoration: 'underline', fontStyle: 'italic' }}>Reading time: {post.readingTime} minute</h3>
+
+                  </Link>
+
+                  <br />
+
+                  <p style={{ marginBottom: '1rem' }}>{post.content}</p>
+
+                </div>
 
               </div>
+            ))}
 
-              <hr />
+          </div>
 
-              <div className={styles.cardBody}>
-
-                <Link href={`/user/${post.username}/posts/${post.slug}`} className={styles.link}>
-
-                  <h2 className={styles.postHeader}>{post.header}</h2>
-                  <h3 style={{ textDecoration: 'underline', fontStyle: 'italic' }}>Reading time: {post.readingTime} minute</h3>
-
-                </Link>
-
-                <br />
-
-                <p style={{ marginBottom: '1rem' }}>{post.content}</p>
-
-              </div>
-
-            </div>
-          ))}
-
-        </div>
-
-      </div >
+        </div >
+      )}
 
       <footer className={styles.footer}>
         <p className={styles.footerContent}>
